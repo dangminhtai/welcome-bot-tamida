7b1a16ea46d44bfe2f3bec03d53c30130b92df75
dangminhtai <dmt826321@gmail.com>
2026-01-26 23:08:07 +0700

    may be add color log

--- bot/Dolia/class/Logger.js
+++ bot/Dolia/class/Logger.js
@@ -1,23 +1,36 @@
 import chalk from 'chalk';
 
 export default class Logger {
+    static getTimestamp() {
+        return chalk.gray(`[${new Date().toLocaleTimeString('vi-VN', { hour12: false })}]`);
+    }
+
     static info(text) {
-        console.log(`${chalk.blue('[INFO]')} ${chalk.gray(new Date().toLocaleTimeString())} : ${text}`);
+        // [ INFO ] 12:00:00 : Client logged in
+        console.log(`${this.getTimestamp()} ${chalk.bgBlue.bold(' INFO ')} ${chalk.blue(text)}`);
     }
 
     static warn(text) {
-        console.log(`${chalk.yellow('[WARN]')} ${chalk.gray(new Date().toLocaleTimeString())} : ${text}`);
+        console.log(`${this.getTimestamp()} ${chalk.bgYellow.black.bold(' WARN ')} ${chalk.yellow(text)}`);
     }
 
     static error(text) {
-        console.log(`${chalk.red('[ERROR]')} ${chalk.gray(new Date().toLocaleTimeString())} : ${text}`);
+        console.log(`${this.getTimestamp()} ${chalk.bgRed.white.bold(' ERROR ')} ${chalk.red(text)}`);
     }
 
     static success(text) {
-        console.log(`${chalk.green('[SUCCESS]')} ${chalk.gray(new Date().toLocaleTimeString())} : ${text}`);
+        console.log(`${this.getTimestamp()} ${chalk.bgGreen.black.bold(' SUCCESS ')} ${chalk.green(text)}`);
     }
 
     static debug(text) {
-        console.log(`${chalk.magenta('[DEBUG]')} ${chalk.gray(new Date().toLocaleTimeString())} : ${text}`);
+        console.log(`${this.getTimestamp()} ${chalk.bgMagenta.white.bold(' DEBUG ')} ${chalk.magenta(text)}`);
+    }
+
+    static client(text) {
+        console.log(`${this.getTimestamp()} ${chalk.bgCyan.black.bold(' CLIENT ')} ${chalk.cyan(text)}`);
+    }
+
+    static system(text) {
+        console.log(`${this.getTimestamp()} ${chalk.bgWhite.black.bold(' SYSTEM ')} ${chalk.white(text)}`);
     }
 }
